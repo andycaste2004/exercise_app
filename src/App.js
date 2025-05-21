a94661dd1de7d20a9fa97cf07b1b7bce19aac0ac
Andres Castellanos <andycaste2004@gmail.com>
2025-05-21 12:35:16 -0600

    Added Youtube search API and almost finish the project, I just need to change the UI as I want and we are done!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import  './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
-import NavBar from './components/NavBar';
+import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <Box width="400" sx={{ width : { x1: '1488px' }}} m="auto">
-        <NavBar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        </Routes>
-        <Footer />
-    </Box>
-  )
-}
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-export default App
-
+export default App;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,14 +1,22 @@
-export const exerciseOptions =  {
-    method: 'GET',
-    headers: {
-    'x-rapidapi-key': '973d26f462msh078711cb07c1cd7p19eb12jsn1c5505632c88',
-    'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-  }
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '973d26f462msh078711cb07c1cd7p19eb12jsn1c5505632c88',
+  },
 };
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options);
-    const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
-    return data;
-}
+  return data;
+};
